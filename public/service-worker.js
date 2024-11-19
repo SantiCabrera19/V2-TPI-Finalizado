547247d06a798e1ba1febc2030f84013711496d5
santicabrera19 <cabrerasanty321@gmail.com>
2024-11-19 20:45:44 -0300

    Try #05 - modo standalone

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,6 +15,22 @@ self.addEventListener('install', (event) => {
         ]);
       })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,19 +4,21 @@ export default function Document() {
   return (
     <Html lang="es">
       <Head>
-        <link rel="manifest" href="manifest.json" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Tienda de productos informáticos" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#fdbeff" />
+        <meta name="description" content="Spa y centro de bienestar" />
         
-        {/* Soporte para iOS */}
+        {/* Soporte mejorado para iOS */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-        <meta name="apple-mobile-web-app-title" content="TPI" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Spa" />
         <link rel="apple-touch-icon" href="/LogoTPI-192x192.png" />
+        <link rel="apple-touch-startup-image" href="/LogoTPI-512x512.png" />
         
         {/* Soporte para Windows */}
         <meta name="msapplication-TileImage" content="/LogoTPI-512x512.png" />
-        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="msapplication-TileColor" content="#fdbeff" />
+        <meta name="msapplication-navbutton-color" content="#fdbeff" />
       </Head>
       <body>
         <Main />
