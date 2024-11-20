3e78d181cc89711b5cd1ae49e6bf224d5a687d57
santicabrera19 <cabrerasanty321@gmail.com>
2024-11-19 21:07:06 -0300

    GetBack del try #07

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,22 +4,8 @@ export default function Document() {
   return (
     <Html lang="es">
       <Head>
-        <link rel="manifest" href="/manifest.json" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="theme-color" content="#fdbeff" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        
-        {/* iOS */}
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-title" content="Spa" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <link rel="apple-touch-icon" href="/LogoTPI-192x192.png" />
-        
-        {/* Splash screens para iOS */}
-        <link rel="apple-touch-startup-image" href="/LogoTPI-512x512.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" />
-        <link rel="apple-touch-startup-image" href="/LogoTPI-512x512.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" />
-        <link rel="apple-touch-startup-image" href="/LogoTPI-512x512.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)" />
-        <link rel="apple-touch-startup-image" href="/LogoTPI-512x512.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" />
       </Head>
       <body>
         <Main />
